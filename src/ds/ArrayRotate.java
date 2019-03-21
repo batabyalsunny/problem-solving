fbe7f35ff467dfe2e2ea678afd2051c57ac949be
Sunny Batabyal <batabyalsunny@gmail.com>
2019-03-21 09:22:51 +0530

    Array rotation in O(n) - O(1)

--- src/ds/ArrayRotate.java
+++ src/ds/ArrayRotate.java
@@ -2,17 +2,40 @@ package ds;
 
 public class ArrayRotate {
 
+	public int gcd(int a, int b) {
+
+		if (0 == b)
+			return a;
+		else
+			return gcd(b, a % b);
+	}
+
 	public static void main(String[] args) {
 
-		int arr[] = { 1, 4, 3, 6, 5, 2, 9, 4 }, d = 3, temp, n;
+		int arr[] = { 1, 4, 3, 6, 5, 2, 9, 4 }, d = 3, temp, n, j, k;
 
 		n = arr.length;
 
-		for (int i = 0; i < d; i++) {
+		ArrayRotate ar = new ArrayRotate();
+		int gcd = ar.gcd(n, d);
+
+		for (int i = 0; i < gcd; i++) {
 
 			temp = arr[i];
-			arr[i] = arr[n - d - i - 1];
-			arr[n - d - i - 1] = temp;
+			j = i;
+			while (true) {
+
+				k = j + d;
+				if (k >= n)
+					k = k - n;
+
+				if (k == i)
+					break;
+
+				arr[j] = arr[k];
+				j = k;
+			}
+			arr[j] = temp;
 		}
 
 		for (int i : arr)
