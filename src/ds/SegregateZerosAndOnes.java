febf16e6389cbaea9812768155fa9a95962f23b7
Sunny Batabyal <batabyalsunny@gmail.com>
2019-03-21 10:23:23 +0530

    Segregate Zeros And Ones in O(n) - O(1)

---
+++ src/ds/SegregateZerosAndOnes.java
@@ -0,0 +1,21 @@
+package ds;
+
+public class SegregateZerosAndOnes {
+
+	public static void main(String[] args) {
+
+		int arr[] = { 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0 }, n = arr.length, i, j = 0;
+
+		for (i = 0; i < n; i++)
+			if (arr[i] == 0) {
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+				j++;
+			}
+
+		for (int k : arr)
+			System.out.println(k);
+	}
+
+}
