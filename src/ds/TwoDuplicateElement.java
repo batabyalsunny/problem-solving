b6903fafa8e66b8d6507d2ad2ce8eda25f3a600a
Sunny Batabyal <batabyalsunny@gmail.com>
2019-04-01 01:36:00 +0530

    Finding two repeating elements in a given array

---
+++ src/ds/TwoDuplicateElement.java
@@ -0,0 +1,23 @@
+package ds;
+
+public class TwoDuplicateElement {
+
+	public static void main(String[] args) {
+
+		int arr[] = { 1, 1, 2, 3, 2 }, n;
+
+		n = arr.length;
+
+		for (int i = 0; i < n; i++) {
+
+			int key = arr[Math.abs(arr[i])];
+
+			if (0 > key)
+				System.out.println(-key);
+			else
+				arr[Math.abs(arr[i])] = -key;
+		}
+
+	}
+
+}
