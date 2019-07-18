977c1b8c6dec701d42d7bfc63fd0eea396237b72
Sunny Batabyal <batabyalsunny@gmail.com>
2019-07-18 13:24:17 +0530

    Detect loop in LL in O(n) - O(1)

--- src/ds/linkedlist/DetectLoopInLL.java
+++ src/ds/linkedlist/DetectLoopInLL.java
@@ -2,8 +2,54 @@ package ds.linkedlist;
 
 public class DetectLoopInLL {
 
+	Node head;
+
+	class Node {
+
+		int data;
+		Node next;
+
+		Node(int data) {
+			this.data = data;
+			next = null;
+		}
+	}
+
+	public void push(int data) {
+
+		Node n = new Node(data);
+
+		n.next = head;
+
+		head = n;
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		DetectLoopInLL llist = new DetectLoopInLL();
+
+		llist.push(20);
+		llist.push(4);
+		llist.push(15);
+		llist.push(10);
+		llist.push(30);
+		llist.push(45);
+		llist.push(35);
+
+		/* Create loop for testing */
+		llist.head.next.next.next.next.next = llist.head.next.next;
+
+		Node p = llist.head, q = llist.head;
+
+		while (null != p && null != q && null != p.next) {
+			p = p.next;
+			q = q.next.next;
+
+			if (p == q) {
+				System.out.println("Loop detected. at " + p.data);
+				break;
+			}
+		}
 
 	}
 
