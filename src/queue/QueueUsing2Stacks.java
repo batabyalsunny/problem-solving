29b088669caab95f202b5ec49b80476c57eb11cc
Sunny Batabyal <batabyalsunny@gmail.com>
2019-08-03 13:12:45 +0530

    Queue using 2 stacks

---
+++ src/queue/QueueUsing2Stacks.java
@@ -0,0 +1,79 @@
+/**
+ * 
+ */
+package queue;
+
+import java.util.Iterator;
+import java.util.Stack;
+
+/**
+ * @author sunnyb
+ *
+ */
+public class QueueUsing2Stacks<E> {
+
+	Stack<E> stack = new Stack<>();
+	Stack<E> tempStack = new Stack<>();
+
+	public void enqueue(E item) {
+
+		while (!stack.isEmpty()) {
+			// Move everything to secondary stack.
+			tempStack.push(stack.pop());
+		}
+
+		// Insert item in stack.
+		stack.push(item);
+
+		// Bring everything back from temp stack to main stack.
+		while (!tempStack.isEmpty()) {
+			stack.push(tempStack.pop());
+		}
+
+	}
+
+	public E dequeue() {
+
+		if (stack.isEmpty()) {
+			System.out.println("Empty");
+			return null;
+		}
+
+		return stack.pop();
+	}
+
+	public void printQueue() {
+
+		Iterator<E> itr = stack.iterator();
+
+		while (itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		QueueUsing2Stacks<Integer> queue = new QueueUsing2Stacks<>();
+
+		queue.enqueue(1);
+		queue.enqueue(2);
+		queue.enqueue(3);
+		queue.enqueue(4);
+		queue.enqueue(5);
+		queue.enqueue(6);
+
+		queue.printQueue();
+
+		System.out.println("====================");
+
+		queue.dequeue();
+		queue.dequeue();
+		queue.dequeue();
+
+		queue.printQueue();
+	}
+
+}
