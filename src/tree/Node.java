b48125940df6d25c14cd0ee736c8c47529c427bd
Sunny Batabyal <batabyalsunny@gmail.com>
2019-08-05 10:27:44 +0530

    Tree traversals

---
+++ src/tree/Node.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package tree;
+
+/**
+ * @author sunnyb
+ *
+ */
+public class Node {
+
+	int data;
+	Node right;
+	Node left;
+
+	/**
+	 * @param data
+	 */
+	public Node(int data) {
+		this.data = data;
+		right = left = null;
+	}
+}
---
+++ src/tree/Traversal.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package tree;
+
+/**
+ * @author sunnyb
+ *
+ */
+public class Traversal {
+
+	Node root;
+
+	public void inorder(Node root) {
+
+		if (root == null) {
+			return;
+		}
+
+		inorder(root.left);
+		System.out.println(root.data);
+		inorder(root.right);
+	}
+
+	public void preorder(Node root) {
+
+		if (root == null) {
+			return;
+		}
+
+		System.out.println(root.data);
+		preorder(root.left);
+		preorder(root.right);
+	}
+
+	public void postorder(Node root) {
+
+		if (root == null) {
+			return;
+		}
+
+		postorder(root.left);
+		postorder(root.right);
+		System.out.println(root.data);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Traversal tree = new Traversal();
+
+		tree.root = new Node(1);
+		tree.root.left = new Node(2);
+		tree.root.right = new Node(3);
+		tree.root.left.left = new Node(4);
+		tree.root.left.right = new Node(5);
+		tree.root.right.left = new Node(6);
+		tree.root.right.right = new Node(7);
+
+		System.out.println("============Inorder============");
+		tree.inorder(tree.root);
+		System.out.println("============Preorder============");
+		tree.preorder(tree.root);
+		System.out.println("============Postorder============");
+		tree.postorder(tree.root);
+	}
+
+}
